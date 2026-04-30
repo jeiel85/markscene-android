d612d4571b6d3087ad76184de97578e4f8aa8231
jeiel <jeiel@briodo.app>
2026-04-30 21:34:38 +0900

    chore: 릴리즈 버전 업데이트 (v0.4.2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.markscene.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.4.1"
+        versionCode = 4
+        versionName = "0.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
