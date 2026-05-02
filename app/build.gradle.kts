77e0ed869c71288373273a24617c46f5e344377b
jeiel <jeiel@briodo.app>
2026-05-02 13:00:45 +0900

    fix: 앱 실행 오류 수정 및 버전 정규화 (v2.0.1) 🛠️

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.markscene.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 200
-        versionName = "2.0.0"
+        versionCode = 201
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
